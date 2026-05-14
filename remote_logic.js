6a110e75e5911f8a5c4b731f7aa7207f25224344
ubase00070 <ubase00070@gmail.com>
2026-05-14 17:36:04 +0900

    Update remote_logic.js

--- remote_logic.js
+++ remote_logic.js
@@ -1194,7 +1194,7 @@
 
     // 팝업 열 때만 생성
     function toggleBattery() {
-        if (batteryPopup.style.display === 'block') {
+        if (batteryPopup.style.display !== 'block') {
 
             // iframe이 없으면 그때 생성
             config.batteryIds.forEach(c => {
