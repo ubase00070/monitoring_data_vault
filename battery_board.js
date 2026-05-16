c12215dbc880637295e455d69056e83ea3baa072
ubase00070 <ubase00070@gmail.com>
2026-05-16 11:48:33 +0900

    Update battery_board.js

--- battery_board.js
+++ battery_board.js
@@ -271,7 +271,7 @@
             display:none; position:absolute; top:0; left:0; right:0; bottom:0;
             background:var(--sur2); border:1px solid var(--bd2);
             border-radius:12px; box-shadow:0 16px 48px rgba(0,0,0,.85);
-            z-index:10; padding:16px; overflow-y:auto;
+            z-index:9999999999; padding:16px; overflow-y:auto;
             font-size:12px; line-height:1.7; color:var(--tx);
         }
         #bb-info-panel.open { display:block; }
