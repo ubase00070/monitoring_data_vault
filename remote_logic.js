87c15c7f861323b6e38eafec55d0d202bc0731ce
ubase00070 <ubase00070@gmail.com>
2026-05-30 16:19:31 +0900

    Update remote_logic.js

--- remote_logic.js
+++ remote_logic.js
@@ -11,15 +11,12 @@
         SECTION 1. 상태 및 설정
        ============================================================ */
     const isHandoverPage = () =>
-        (location.href.includes('go.neubie.ai/ko/remote/multiple') &&
-        !location.href.includes('/driving')) ||
-        location.href.includes('multimonitoring.vercel.app');
-
-    // 밝기바를 표시할 페이지 조건 (multiple + driving 모두 포함)
-    const isBrightnessPage = () =>
-        (location.href.includes('go.neubie.ai/ko/remote/multiple') &&
-        !location.href.includes('/driving')) ||
-        location.href.includes('multimonitoring.vercel.app');
+	    location.href.includes('go.neubie.ai/ko/remote/multiple') &&
+	    !location.href.includes('/driving');
+	
+	const isBrightnessPage = () =>
+	    location.href.includes('go.neubie.ai/ko/remote/multiple') &&
+	    !location.href.includes('/driving');
     
        const config = {
         targetIds: ['44', '56', '65', '109'],
