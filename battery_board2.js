1ae25d09de213e59bc3a7348b37ec36bedb0682f
ubase00070 <ubase00070@gmail.com>
2026-07-01 16:58:33 +0900

    Update battery_board2.js

--- battery_board2.js
+++ battery_board2.js
@@ -352,7 +352,7 @@
             display:none; position:fixed;
             top:50%; left:50%; transform:translate(-50%,-50%);
             width:368px; background:#0a0a0c;
-            border:1px solid var(--bd2); border-radius:12px;
+            border:2px solid rgba(255,255,255,.85); border-radius:12px;
             box-shadow:0 16px 48px rgba(0,0,0,.9);
             z-index:999999999; font-family:'Lato',sans-serif;
             color:var(--tx); overflow:hidden;
