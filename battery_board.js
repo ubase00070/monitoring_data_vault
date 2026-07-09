5e0e81580fd8e3d936d48d31c89a8df09a647b31
Snoopy <ubase00070@gmail.com>
2026-07-09 10:09:09 +0900

    Update battery_board.js

--- battery_board.js
+++ battery_board.js
@@ -54,7 +54,7 @@
             background-clip: padding-box, border-box;
             box-shadow:0 24px 60px rgba(0,0,0,.75);
             z-index:9999999; font-family:'Pretendard','Lato',-apple-system,BlinkMacSystemFont,'Segoe UI',sans-serif;
-            font-weight:500;
+            font-weight:700;
 			color:var(--tx); flex-direction:column;
         }
         #bb.open { display:flex; }
