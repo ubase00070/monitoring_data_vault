b94269f9ed6621cb0503b77213ae6952f681d5f4
Snoopy <ubase00070@gmail.com>
2026-07-14 13:32:35 +0900

    Update battery_board.js

--- battery_board.js
+++ battery_board.js
@@ -633,9 +633,9 @@
     ];
 
     const MONITOR_GROUPS = [
-        { id:'yeoksam',  label:'역삼 요기요',    keywords:['역삼동 (요기요)'] },
-        { id:'songdo',   label:'송도 요기요',    keywords:['송도 신도시(요기요)'] },
-        { id:'seongsu',  label:'성수 요기요',    keywords:['성수 요기요'] },
+        { id:'yeoksam',  label:'역삼 요기요',    keywords:['역삼동'] },
+        { id:'songdo',   label:'송도 요기요',    keywords:['송도 신도시'] },
+        { id:'seongsu',  label:'성수 요기요',    keywords:['성수동'] },
         { id:'seongnam', label:'성남 삼평/서현', keywords:['성남형'] },
     ];
 
